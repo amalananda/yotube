7009e99329c2e9b6a773edaa70c6a4bc84bf40a4
amala <amalananda332@gmail.com>
2024-11-12 23:29:44 +0700

    feat: created follower schema and models

---
+++ server/models/Follower.js
@@ -0,0 +1,77 @@
+const database = require("../config/mongo")
+const { ObjectId } = require('mongodb')
+
+class Follower {
+  static async findAllUserFollower(userId) {
+    try {
+      // Pastikan koneksi berhasil
+      const db = await database()
+      const followersCollection = db.collection("followers")
+      // Ubah id string menjadi ObjectId
+      const followers = await followersCollection.find({ followerId: new ObjectId(userId) }).toArray()
+      // console.log("userId", userId)
+      // console.log("followers", followers)
+
+      // Konversi _id menjadi string untuk setiap user
+      return followers.map(follower => ({
+        ...follower,
+        _id: follower._id.toString()
+      }))
+    } catch (err) {
+      console.error("Error in findAll:", err)
+      throw new Error("Error fetching all users")
+    }
+  }
+  static async findAllUserFollowing(userId) {
+    try {
+      // Pastikan koneksi berhasil
+      const db = await database()
+      const followingCollection = db.collection("followers")
+
+      // Ubah id string menjadi ObjectId
+      const following = await followingCollection.find({ followingId: new ObjectId(userId) }).toArray()
+
+      // Konversi _id menjadi string untuk setiap user
+      return following.map(following => ({
+        ...following,
+        _id: following._id.toString()
+      }))
+    } catch (err) {
+      console.error("Error in findAll:", err)
+      throw new Error("Error fetching all users")
+    }
+  }
+  static async countFollowing(userId) {
+    try {
+      const db = await database()
+      const followersCollection = db.collection("followers")
+
+      const result = await followersCollection.aggregate([
+        { $match: { followerId: new ObjectId(userId) } },
+        { $count: "totalFollowing" }
+      ]).toArray()
+
+      // Jika hasilnya kosong, berarti following-nya 0
+      return result[0]?.totalFollowing || 0
+    } catch (err) {
+      console.error("Error in countFollowing:", err)
+      throw new Error("Error counting following")
+    }
+  }
+
+
+  static async createFollow(followerId, followingId) {
+    const db = await database()
+    const followersCollection = db.collection("followers")
+    const newFollow = {
+      followerId: new ObjectId(followerId),
+      followingId: new ObjectId(followingId),
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+    const result = await followersCollection.insertOne(newFollow)
+    return { _id: result.insertedId, ...newFollow }
+  }
+}
+
+module.exports = Follower
---
+++ server/schema/follower.js
@@ -0,0 +1,72 @@
+const Follower = require("../models/Follower")
+
+const typeDefs = `#graphql
+
+type Follow {
+    _id: ID!
+    followingId: ID!
+    followerId: ID!
+    createdAt: String
+    updatedAt: String
+  }
+  type Query {
+    followers(userId: ID!): [Follow]        # Mendapatkan daftar pengguna yang mengikuti user tertentu
+    following(userId: ID!): [Follow]        # Mendapatkan daftar pengguna yang diikuti oleh user tertentu
+    followersCount(userId: ID!): Int
+    followingCount(userId: ID!): Int
+  }
+  type Mutation {
+    # addFollower(user:UserInput!):User,
+    # addFollowing
+    followUser(followerId: ID!, followingId: ID!): Follow
+    unfollowUser(followerId: ID!, followingId: ID!): String
+  }
+
+  input FollowerInput {
+    followingId: ID!
+    followerId: ID!
+  }
+  input FollowingInput {
+    followingId: ID!
+    followerId: ID!
+  }
+`
+
+const resolvers = {
+  Query: {
+    followers: async (_, { userId }) => {
+      try {
+        return await Follower.findAllUserFollower(userId)
+      } catch (err) {
+        console.error("Error in followers:", err)
+        throw new Error("Failed to fetch followers")
+      }
+    },
+    following: async (_, { userId }) => {
+      try {
+        return await Follower.findAllUserFollowing(userId)
+      } catch (err) {
+        console.error("Error in following:", err)
+        throw new Error("Failed to fetch following")
+      }
+    },
+    followersCount: async (_, { userId }) => {
+      return await Follower.countFollowers(userId)
+    },
+    followingCount: async (_, { userId }) => {
+      return await Follower.countFollowing(userId)
+    },
+  },
+
+  Mutation: {
+    followUser: async (_, { followerId, followingId }) => {
+      try {
+        return await Follower.createFollow(followerId, followingId)
+      } catch (err) {
+        console.error("Error in followUser:", err)
+        throw new Error("Failed to follow user")
+      }
+    },
+  }
+}
+module.exports = { typeDefs, resolvers }
