06d9d53948a312b909dcdfcc027b2be9c42f87df
amala <amalananda332@gmail.com>
2024-11-14 23:26:51 +0700

    fix: fixed code for user

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,67 @@
 const database = require("../config/mongo")
 const { ObjectId } = require('mongodb')
+const { hashPassword, comparePassword } = require("../helpers/bcrypt")
+const { signToken } = require('../helpers/jwt')
 
 class User {
+  static async register(name, username, email, password) {
+    try {
+      const db = await database()
+      const usersCollection = db.collection("users")
+
+      // Cek apakah email sudah terdaftar
+      const emailExist = await usersCollection.findOne({ email })
+      if (emailExist) {
+        console.log("Email Exist Check: ", emailExist)
+        throw new Error("Email sudah terdaftar.")  // Lempar error jika email sudah ada
+      }
+
+      const passwordHash = hashPassword(password)
+      const result = await usersCollection.insertOne({
+        name,
+        username,
+        email,
+        password: passwordHash, // Simpan password yang sudah di-hash
+      })
+
+      return {
+        _id: result.insertedId.toString(),
+        name,
+        username,
+        email, // Mengembalikan user baru dengan ID
+      }
+    } catch (err) {
+      console.error("Error in register:", err)
+      throw new Error("Gagal mendaftarkan pengguna baru")
+    }
+  }
+
+  static async login(email, password) {
+    try {
+      const db = await database()
+      const usersCollection = db.collection("users")
+
+      const user = await usersCollection.findOne({ email })
+      if (!user) {
+        throw new Error("Pengguna tidak ditemukan.")  // Lempar error jika user tidak ditemukan
+      }
+
+      // Cek apakah password sesuai
+      const isPasswordValid = comparePassword(password, user.password) // Pastikan compare menggunakan bcrypt
+      if (!isPasswordValid) {
+        throw new Error("Password salah.")  // Lempar error jika password salah
+      }
+      const payload = { id: user._id, email: user.email }
+      const token = signToken(payload)
+      return {
+        access_token: token,
+      }
+    } catch (err) {
+      console.error("Error in login:", err)
+      throw new Error("Gagal masuk")
+    }
+  }
+
   static async findAll() {
     try {
       // Pastikan koneksi berhasil
@@ -24,23 +84,25 @@ class User {
 
   static async findById(id) {
     try {
-      // Pastikan koneksi berhasil
       const db = await database()
       const usersCollection = db.collection("users")
       const result = await usersCollection.aggregate([
-        { $match: { _id: new ObjectId(String(id)) } },
+        { $match: { _id: new ObjectId(id) } },
         {
           $lookup: {
-            from: "posts",           // Nama koleksi yang ingin di-join
-            localField: "_id",       // Field di koleksi users
-            foreignField: "authorId", // Field di koleksi posts yang berhubungan
-            as: "posts"              // Alias hasil join
+            from: "posts",
+            localField: "_id",
+            foreignField: "authorId",
+            as: "posts"
           }
         }
       ]).toArray()
+
       if (result.length > 0) {
         const user = result[0]
-        user._id = user._id() // Konversi _id ke string
+        user._id = user._id.toString() // Konversi _id ke string
+        user.followers = await this.follower(id)  // Mendapatkan followers
+        user.followings = await this.following(id) // Mendapatkan followings
         return user
       } else {
         return null
@@ -76,23 +138,74 @@ class User {
   static async follower(userId) {
     try {
       const db = await database()
-      const userCollection = db.collection("users")
+      const userCollection = db.collection("followers")
       const result = await userCollection.aggregate([
-        { $match: { _id: new ObjectId(userId) } },
+        { $match: { followingId: new ObjectId(userId) } },
         {
           $lookup: {
-            from: "followers",
-            localField: "_id",
-            foreignField: "followingId",
-            as: "followers"
+            from: "users",
+            localField: "followerId",
+            foreignField: "_id",
+            as: "user"
+          }
+        },
+        {
+          $unwind: "$user"
+        },
+        {
+          $project: {
+            _id: "$user._id",
+            name: "$user.name",
+            username: "$user.username"
           }
         }
       ]).toArray()
-      console.log("RESULT", result)
-      return result[0]
+
+      // Konversi _id pengguna di followers menjadi string
+      return result.map(follower => ({
+        ...follower,
+        _id: follower._id.toString()
+      }))
     } catch (err) {
-      console.error("Error in countFollowing:", err)
-      throw new Error("Error counting following")
+      console.error("Error in follower:", err)
+      throw new Error("Error fetching followers")
+    }
+  }
+
+  static async following(userId) {
+    try {
+      const db = await database()
+      const userCollection = db.collection("followers")
+      const result = await userCollection.aggregate([
+        { $match: { followerId: new ObjectId(userId) } },
+        {
+          $lookup: {
+            from: "users",
+            localField: "followingId",
+            foreignField: "_id",
+            as: "user"
+          }
+        },
+        {
+          $unwind: "$user"
+        },
+        {
+          $project: {
+            _id: "$user._id",
+            name: "$user.name",
+            username: "$user.username"
+          }
+        }
+      ]).toArray()
+
+      // Konversi _id pengguna di followings menjadi string
+      return result.map(following => ({
+        ...following,
+        _id: following._id.toString()
+      }))
+    } catch (err) {
+      console.error("Error in following:", err)
+      throw new Error("Error fetching followings")
     }
   }
 }
--- server/schema/user.js
+++ server/schema/user.js
@@ -1,4 +1,6 @@
 const User = require("../models/User")
+const { GraphQLError } = require('graphql')
+
 
 // Definisi schema menggunakan #graphql, tanpa gql
 const typeDefs = `#graphql
@@ -8,6 +10,7 @@ type User {
     username: String!
     email: String!
     password: String!
+    post: [Post]
     followers: [User!]!
     followings: [User!]!
 }
@@ -15,10 +18,15 @@ type Query {
     users: [User]
     userById(id: ID!): User
     searchUsers(query: String!): [User]
-    getUserWithFollowersAndFollowing(userId: ID!): User
+    # getUserWithFollowersAndFollowing(userId: ID!): User
 }
+type Token{
+  access_token: String!
+}
+
 type Mutation {
-    addUser(user: UserInput!): User
+  registerUser(user: UserInput!): User
+  loginUser(email: String!, password: String!): Token
 }
 input UserInput {
     name: String
@@ -55,20 +63,74 @@ const resolvers = {
         throw err
       }
     },
-    getUserWithFollowersAndFollowing: async (_, { userId }) => {
-      return await User.follower(userId)
-    },
+    // getUserWithFollowersAndFollowing: async (_, { userId }) => {
+    //   return await User.follower(userId)
+    // },
   },
   Mutation: {
-    addUser: async (_, { user }) => {
+    registerUser: async (_, { user }) => {
+      const { name, username, email, password } = user
       try {
-        const newUser = await User.create(user)
+        const newUser = await User.register(name, username, email, password)
         return newUser
       } catch (err) {
-        throw new Error("Failed to add new user")
+        console.error("Error during user registration:", err.message) // Log error lebih jelas
+
+        // Jika email sudah terdaftar
+        if (err.message === "Email sudah terdaftar.") {
+          throw new GraphQLError("Email sudah terdaftar.", {
+            extensions: {
+              code: "BAD_REQUEST",
+              http: { status: 400 },
+            },
+          })
+        }
+
+        // Untuk error lainnya, beri penanganan lebih jelas
+        throw new GraphQLError("Gagal mendaftarkan pengguna baru.", {
+          extensions: {
+            code: "INTERNAL_SERVER_ERROR",
+            http: { status: 500 },
+          },
+        })
       }
-    }
-  }
+    },
+    loginUser: async (_, { email, password }) => {
+      try {
+        const accessToken = await User.login(email, password)
+        return accessToken
+      } catch (err) {
+        if (err.message === "Pengguna tidak ditemukan.") {
+          throw new GraphQLError("Pengguna tidak ditemukan.", {
+            extensions: {
+              code: "BAD_REQUEST",
+              http: {
+                status: 400,
+              },
+            },
+          })
+        } else if (err.message === "Password salah.") {
+          throw new GraphQLError("Password salah.", {
+            extensions: {
+              code: "BAD_REQUEST",
+              http: {
+                status: 400,
+              },
+            },
+          })
+        }
+        throw new GraphQLError("Gagal masuk.", {
+          extensions: {
+            code: "INTERNAL_SERVER_ERROR",
+            http: {
+              status: 500,
+            },
+          },
+        })
+      }
+    },
+  },
+
 }
 
 module.exports = { typeDefs, resolvers }
